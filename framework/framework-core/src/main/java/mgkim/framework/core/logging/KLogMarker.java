9f265e5bee2238159ec9724597c9d4757aba192f
김명구 <hi@mgkim.net>
2022-01-09 17:40:30 +0900

    (refactoing) logback

---
+++ framework/framework-core/src/main/java/mgkim/framework/core/logging/KLogMarker.java
@@ -0,0 +1,14 @@
+package mgkim.framework.core.logging;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class KLogMarker {
+	
+	public static final Marker REQUEST = MarkerFactory.getMarker("[ +++ REQUEST +++ ]");
+	public static final Marker RESPONSE = MarkerFactory.getMarker("[ --- RESPONSE --- ]");
+	public static final Marker SQL = MarkerFactory.getMarker("[ *** sql *** ]");
+	public static final Marker request = MarkerFactory.getMarker("[ *** request *** ]");
+	public static final Marker response = MarkerFactory.getMarker("[ *** response *** ]");
+	
+}
---
+++ service/service-lib/src/main/java/mgkim/service/lib/env/CmmProfile.java
@@ -0,0 +1,31 @@
+package mgkim.service.lib.env;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import mgkim.framework.core.env.KConstant;
+import mgkim.framework.core.env.KProfile;
+import mgkim.service.lib.type.TSiteType;
+
+public class CmmProfile {
+	
+	public static void init(String defAppName) {
+		// 사이트 구분 설정 (www, adm, bat)
+		{
+			String appName = null;
+			if (System.getProperty(KConstant.VM_APP_NAME) == null) {
+				appName = defAppName;
+				System.setProperty(KConstant.VM_APP_NAME, appName);
+			}
+			String[] arr = java.util.Arrays.stream(TSiteType.values()).map(item -> item.label()).toArray(String[]::new);
+			Matcher matcher = Pattern.compile("("+String.join("|", arr)+")[0-9]{1,2}").matcher(appName);
+			if (matcher.find()) {
+				TSiteType site = TSiteType.get(matcher.group(1));
+				KProfile.SITE_TPCD = site.code();
+				KProfile.addProfile(site.label());
+			} else {
+				System.exit(-1);
+			}
+		}
+	}
+}
