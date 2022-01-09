4626113a2672bbe7d18cfc15be70d65d084a4160
김명구 <hi@mgkim.net>
2022-01-10 02:07:49 +0900

    (fix) 로그인 userId 필드에 기본 예시값 추가

--- service/service-www/src/main/java/mgkim/service/www/api/cmm/userlogin/vo/UserLoginReqVO.java
+++ service/service-www/src/main/java/mgkim/service/www/api/cmm/userlogin/vo/UserLoginReqVO.java
@@ -2,12 +2,15 @@ package mgkim.service.www.api.cmm.userlogin.vo;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @JsonSerialize
 public class UserLoginReqVO {
 
 	private String userId;
 	private String email;
-
+	
+	@ApiModelProperty(name = "userId", position = 1, dataType = "string", example = "1000000001")
 	public String getUserId() {
 		return userId;
 	}
