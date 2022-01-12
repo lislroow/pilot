27201d40d9c391e7067a70918e4072e6e7be7077
김명구 <hi@mgkim.net>
2022-01-12 21:36:29 +0900

    (fix) build.sh 코드 정리

--- service/service-www/script/build.sh
+++ service/service-www/script/build.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
 ## env
-. ./env.sh
+UNAME=`uname -s`
+if [[ "${UNAME}" = "Linux"* ]]; then
+  OS_NAME="linux"
+elif [[ "${UNAME}" = "CYGWIN"* || "${UNAME}" = "MINGW"* ]]; then
+  OS_NAME="win"
+fi
 
-PROJECT_BASE="$( cd $( ${DIRNAME_CMD} "$0" )/.. && pwd -P)"
-echo "${PROJECT_BASE}"
+case ${OS_NAME} in
+  linux)
+    JAVA_HOME=/prod/java/openjdk-11.0.13.8-temurin
+    M2_HOME=/prod/maven/maven
+    PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
+    ;;
+  win)
+    JAVA_HOME=/z/develop/java/openjdk-11.0.13.8-temurin
+    M2_HOME=/z/develop/build/maven-3.6.3
+    PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
+    ;;
+  *)
+    echo "invalid os"
+    exit -1
+    ;;
+esac
 
-PROFILE_SYS=dev
+SCRIPT_DIR="$( cd $( dirname "$0" ) && pwd -P)"
+BASEDIR="$( cd ${SCRIPT_DIR}/.. && pwd -P)"
 
 printf '%s\n' $(cat << EOF
 UNAME=${UNAME}
 OS_NAME=${OS_NAME}
-M2_HOME=${M2_HOME}
 JAVA_HOME=${JAVA_HOME}
-APP_HOME=${APP_HOME}
-APP_NAME=${JAVA_HOME}
-APP_ID=${APP_ID}
-SERVER_PORT=${SERVER_PORT}
-PROFILE_SYS=${PROFILE_SYS}
-PROJECT_BASE=${PROJECT_BASE}
+M2_HOME=${M2_HOME}
+SCRIPT_DIR=${SCRIPT_DIR}
+BASEDIR=${BASEDIR}
 EOF
 )
 
@@ -27,7 +43,7 @@ function build() {
   echo "+++ (build) build maven project +++"
   
   MVN_ARGS=""
-  MVN_ARGS="${MVN_ARGS} --file ${PROJECT_BASE}/pom.xml"
+  MVN_ARGS="${MVN_ARGS} --file ${BASEDIR}/pom.xml"
   MVN_ARGS="${MVN_ARGS} -Dfile.encoding=utf-8"
   MVN_ARGS="${MVN_ARGS} -Dmaven.test.skip=true"
   MVN_ARGS="${MVN_ARGS} --update-snapshots"
@@ -41,30 +57,11 @@ function build() {
   JAR_FILE=$(xmlstarlet sel -N x="http://maven.apache.org/POM/4.0.0" \
     -t -v \
     "concat(x:project/x:artifactId, '-', x:project/x:version, '.', x:project/x:packaging)" \
-    ${PROJECT_BASE}/pom.xml)
-  
+    ${BASEDIR}/pom.xml)
   echo "JAR_FILE=${JAR_FILE}"
-  
-  CHECKSUM=$(${MD5SUM_CMD} ${PROJECT_BASE}/target/${JAR_FILE} | awk '{ print substr($1, 1, 4) }')
-  JAR_MD5_FILE=${JAR_FILE%.*}_${CHECKSUM}.${JAR_FILE##*.}
-  cp ${PROJECT_BASE}/target/${JAR_FILE} ${PROJECT_BASE}/target/${JAR_MD5_FILE}
-  
-  echo "JAR_FILE=${JAR_FILE}"
-  echo "JAR_MD5_FILE=${JAR_MD5_FILE}"
-  
-  JAR_FILE_PTRN=$(xmlstarlet sel -N x="http://maven.apache.org/POM/4.0.0" \
-    -t -v \
-    "concat(x:project/x:artifactId, '-*.', x:project/x:packaging)" \
-    ${PROJECT_BASE}/pom.xml)
-  
-  echo "JAR_FILE_PTRN=${JAR_FILE_PTRN}"
-  
-  if [ ! -e ${PROJECT_BASE}/target/$JAR_MD5_FILE ]; then
-    echo "build output file '${JAR_MD5_FILE}' is not found."
-  fi
   
   # jar 파일명에 "-SNAPSHOT" 이 있으면 snapshot 저장소에 deploy 되어야 합니다. 
-  if [[ "${JAR_MD5_FILE}" = *"-SNAPSHOT"* ]]; then
+  if [[ "${JAR_FILE}" = *"-SNAPSHOT"* ]]; then
     NX_REPO=snapshot
   else
     NX_REPO=release
@@ -72,8 +69,8 @@ function build() {
   
   ## deploy-nexus
   MVN_ARGS=""
-  MVN_ARGS="${MVN_ARGS} -DpomFile=${PROJECT_BASE}/pom.xml"
-  MVN_ARGS="${MVN_ARGS} -Dfile=${PROJECT_BASE}/target/${JAR_MD5_FILE}"
+  MVN_ARGS="${MVN_ARGS} -DpomFile=${BASEDIR}/pom.xml"
+  MVN_ARGS="${MVN_ARGS} -Dfile=${BASEDIR}/target/${JAR_FILE}"
   MVN_ARGS="${MVN_ARGS} --quiet"
   MVN_ARGS="${MVN_ARGS} -DrepositoryId=maven-${NX_REPO}"
   MVN_ARGS="${MVN_ARGS} -Durl=https://nexus/repository/maven-${NX_REPO}/"
@@ -85,42 +82,12 @@ function build() {
   echo "--- (build) build maven project ---"$'\n'
 }
 
-## (transfer) transfer *.sh files
-function transfer() {
-  echo "+++ (transfer) transfer *.sh files +++"
-  
-  case $PROFILE_SYS in
-    dev)
-      SVR_LIST=(
-        '172.28.200.30'
-      )
-      APP_HOME=/app/WAS/pilot
-    ;;
-    sta*)
-    ;;
-  esac
-  echo "SVR_LIST=${SVR_LIST[*]}"
-  echo "APP_HOME=${APP_HOME}"
-  
-  DEPLOY_FILES=(
-    "./*.sh"
-  )
-  echo "DEPLOY_FILES=${DEPLOY_FILES[*]}"
-  
-  for SVR in ${SVR_LIST[*]}
-  do
-    scp ${DEPLOY_FILES[*]} root@${SVR}:${APP_HOME}
-    ssh root@${SVR} "chmod u+x ${APP_HOME}/*.sh;"
-  done
-  
-  echo "--- (transfer) transfer *.sh files ---"$'\n'
-}
 
 ## (deploy) deploying trigger
 function deploy() {
   echo "+++ (deploy) deploying trigger +++"
-  
-  case $PROFILE_SYS in
+  PROFILE_SYS=dev
+  case ${PROFILE_SYS} in
     dev)
       SVR_LIST=(
         '172.28.200.30'
@@ -145,15 +112,11 @@ case $1 in
   build)
     build
     ;;
-  trans*)
-    transfer
-    ;;
   deploy)
     deploy
     ;;
   *)
     build
-    transfer
     deploy
     ;;
 esac
---
+++ service/service-www/script/send.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## env
+SCRIPT_DIR="$( cd $( dirname "$0" ) && pwd -P)"
+
+## (transfer) transfer *.sh files
+function transfer() {
+  echo "+++ (transfer) transfer *.sh files +++"
+  
+  DEPLOY_FILES=(
+    "${SCRIPT_DIR}/*.sh"
+  )
+  echo "DEPLOY_FILES=${DEPLOY_FILES[*]}"
+  
+  for SVR in ${SVR_LIST[*]}
+  do
+    scp ${DEPLOY_FILES[*]} root@${SVR}:${APP_HOME}
+    ssh root@${SVR} "chmod u+x ${APP_HOME}/*.sh;"
+  done
+  
+  echo "--- (transfer) transfer *.sh files ---"$'\n'
+}
+
+PROFILE_SYS=$1
+
+if [ "$1" == "" ]; then
+  PROFILE_SYS=dev
+fi
+
+case $PROFILE_SYS in
+  dev)
+    SVR_LIST=(
+      '172.28.200.30'
+    )
+    APP_HOME="/app/WAS/pilot"
+  ;;
+  sta*)
+    exit -1
+  ;;
+esac
+echo "SVR_LIST=${SVR_LIST[*]}"
+echo "APP_HOME=${APP_HOME}"
+
+transfer "${PROFILE_SYS}"
