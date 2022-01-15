934d735fe688d9265d7cb1e1c678dfe2cc6b004e
김명구 <hi@mgkim.net>
2022-01-15 19:30:45 +0900

    (fix) stop.sh 전체 실행 추가

--- service/script/stop.sh
+++ service/script/stop.sh
@@ -24,63 +24,98 @@ EOF
 ## (stop) stop
 function stop() {
   echo "+++ (stop) stop +++"
-  local ps_cmd="ps -ef | grep -v grep | grep -v tail |  grep -v .sh | grep ${APP_ID} | awk '{ print \$2 }'"
-  echo "ps_cmd=${ps_cmd}"
-  local _pid=$(eval "${ps_cmd}")
-  
-  if [ "${_pid}" != "" ]; then
-    echo "stopping ${APP_ID}(pid:'${_pid}')"
-    local kill_cmd="kill -15 ${_pid}"
-    echo "kill_cmd=${kill_cmd}"
-    eval "${kill_cmd}"
+  for app_id in ${APP_ID_LIST[*]}
+  do
+    local ps_cmd="ps -ef | grep -v grep | grep -v tail |  grep -v .sh | grep ${app_id} | awk '{ print \$2 }'"
+    echo "ps_cmd=${ps_cmd}"
+    local _pid=$(eval "${ps_cmd}")
     
-    i=1
-    while [ $i -lt 600 ];
-    do
-      local _check_pid=$(eval "${ps_cmd}")
-      echo "_check_pid=${_check_pid}"
-      if [ "${_check_pid}" == "" ]; then
-        echo "${APP_ID}(pid:'${_pid}') killed"
-        break
-      fi
-      echo "wait for ${APP_ID}(pid:'${_pid}') killing"
-      i=$(( $i + 1 ))
-      sleep 1
-    done
-  else
-    echo "${APP_ID} is not started"
-  fi
+    if [ "${_pid}" != "" ]; then
+      echo "stopping ${app_id}(pid:'${_pid}')"
+      local kill_cmd="kill -15 ${_pid}"
+      echo "kill_cmd=${kill_cmd}"
+      eval "${kill_cmd}"
+      
+      i=1
+      while [ $i -lt 600 ];
+      do
+        local _check_pid=$(eval "${ps_cmd}")
+        echo "_check_pid=${_check_pid}"
+        if [ "${_check_pid}" == "" ]; then
+          echo "${app_id}(pid:'${_pid}') killed"
+          break
+        fi
+        echo "wait for ${app_id}(pid:'${_pid}') killing"
+        i=$(( $i + 1 ))
+        sleep 1
+      done
+    else
+      echo "${app_id} is not started"
+    fi
+  done
 echo "--- //(stop) stop ---"
 }
 
 
 echo "+++ (runtime-env) +++"
 EXEC_USER="tomcat"
-APP_ID=$1
-case "${APP_ID}" in
+APP_ID_LIST=()
+case "$1" in
+  dev)
+    APP_ID_LIST+=(
+      "dwww11"
+      "dwww12"
+      "dadm11"
+      "dadm12"
+    )
+    ;;
+  sta)
+    APP_ID_LIST+=(
+      "swww11"
+      "swww12"
+      "sadm11"
+      "sadm12"
+    )
+    ;;
+  www)
+    APP_ID_LIST+=(
+      "dwww11"
+      "dwww12"
+      "swww11"
+      "swww12"
+    )
+    ;;
+  adm)
+    APP_ID_LIST+=(
+      "dadm11"
+      "dadm12"
+      "sadm11"
+      "sadm12"
+    )
+    ;;
   dw*1)
-    APP_ID="dwww11"
+    APP_ID_LIST+=("dwww11")
     ;;
   dw*2)
-    APP_ID="dwww12"
+    APP_ID_LIST+=("dwww12")
     ;;
   sw*1)
-    APP_ID="swww11"
+    APP_ID_LIST+=("swww11")
     ;;
   sw*2)
-    APP_ID="swww12"
+    APP_ID_LIST+=("swww12")
     ;;
   da*1)
-    APP_ID="dadm11"
+    APP_ID_LIST+=("dadm11")
     ;;
   da*2)
-    APP_ID="dadm12"
+    APP_ID_LIST+=("dadm12")
     ;;
   sa*1)
-    APP_ID="sadm11"
+    APP_ID_LIST+=("sadm11")
     ;;
   sa*2)
-    APP_ID="sadm12"
+    APP_ID_LIST+=("sadm12")
     ;;
   *)
     exit -1
