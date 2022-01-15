5f28add48d18a1c2d02c170de8360b7f382be3ba
김명구 <hi@mgkim.net>
2022-01-15 19:16:29 +0900

    (fix) build.sh 전체 빌드 추가

--- service/build.sh
+++ service/build.sh
@@ -45,69 +45,75 @@ EOF
 function build() {
   echo "+++ (build) build maven project +++"
   
-  local mvn_args=""
-  mvn_args="${mvn_args} --file ${BASEDIR}/${APP_NAME}/pom.xml"
-  mvn_args="${mvn_args} -Dfile.encoding=utf-8"
-  mvn_args="${mvn_args} -Dmaven.test.skip=true"
-  mvn_args="${mvn_args} --update-snapshots"
-  mvn_args="${mvn_args} --batch-mode"
-  mvn_args="${mvn_args} --quiet"
-  
-  local mvn_cmd="mvn ${mvn_args} clean package spring-boot:repackage"
-  echo "mvn_cmd=${mvn_cmd}"
-  eval "${mvn_cmd}"
-  
-  local jar_file=$(xmlstarlet sel -N x="http://maven.apache.org/POM/4.0.0" \
-    -t -v \
-    "concat(x:project/x:artifactId, '-', x:project/x:version, '.', x:project/x:packaging)" \
-    ${BASEDIR}/${APP_NAME}/pom.xml)
-  echo "jar_file=${jar_file}"
-  
-  # jar 파일명에 "-SNAPSHOT" 이 있으면 snapshot 저장소에 deploy 되어야 합니다.
-  local nx_repo 
-  if [[ "${jar_file}" = *"-SNAPSHOT"* ]]; then
-    nx_repo="snapshot"
-  else
-    nx_repo="release"
-  fi
-  
-  ## deploy-nexus
-  mvn_args=""
-  mvn_args="${mvn_args} -DpomFile=${BASEDIR}/${APP_NAME}/pom.xml"
-  mvn_args="${mvn_args} -Dfile=${BASEDIR}/${APP_NAME}/target/${jar_file}"
-  mvn_args="${mvn_args} --quiet"
-  mvn_args="${mvn_args} -DrepositoryId=maven-${nx_repo}"
-  mvn_args="${mvn_args} -Durl=https://nexus/repository/maven-${nx_repo}/"
-  
-  mvn_cmd="mvn deploy:deploy-file ${mvn_args}"
-  echo "mvn_cmd=${mvn_cmd}"
-  eval "${mvn_cmd}"
+  for app_name in ${APP_NAME_LIST[*]}
+  do
+    local mvn_args=""
+    mvn_args="${mvn_args} --file ${BASEDIR}/${app_name}/pom.xml"
+    mvn_args="${mvn_args} -Dfile.encoding=utf-8"
+    mvn_args="${mvn_args} -Dmaven.test.skip=true"
+    mvn_args="${mvn_args} --update-snapshots"
+    mvn_args="${mvn_args} --batch-mode"
+    mvn_args="${mvn_args} --quiet"
+    
+    local mvn_cmd="mvn ${mvn_args} clean package spring-boot:repackage"
+    echo "mvn_cmd=${mvn_cmd}"
+    eval "${mvn_cmd}"
+    
+    local jar_file=$(xmlstarlet sel -N x="http://maven.apache.org/POM/4.0.0" \
+      -t -v \
+      "concat(x:project/x:artifactId, '-', x:project/x:version, '.', x:project/x:packaging)" \
+      ${BASEDIR}/${app_name}/pom.xml)
+    echo "jar_file=${jar_file}"
+    
+    # jar 파일명에 "-SNAPSHOT" 이 있으면 snapshot 저장소에 deploy 되어야 합니다.
+    local nx_repo 
+    if [[ "${jar_file}" = *"-SNAPSHOT"* ]]; then
+      nx_repo="snapshot"
+    else
+      nx_repo="release"
+    fi
+    
+    ## deploy-nexus
+    mvn_args=""
+    mvn_args="${mvn_args} -DpomFile=${BASEDIR}/${app_name}/pom.xml"
+    mvn_args="${mvn_args} -Dfile=${BASEDIR}/${app_name}/target/${jar_file}"
+    mvn_args="${mvn_args} --quiet"
+    mvn_args="${mvn_args} -DrepositoryId=maven-${nx_repo}"
+    mvn_args="${mvn_args} -Durl=https://nexus/repository/maven-${nx_repo}/"
+    
+    mvn_cmd="mvn deploy:deploy-file ${mvn_args}"
+    echo "mvn_cmd=${mvn_cmd}"
+    eval "${mvn_cmd}"
+  done
   
   echo "--- //(build) build maven project ---"
 }
 
 
 echo "+++ (runtime-env) +++"
-APP_NAME=$1
-case "${APP_NAME}" in
+APP_NAME_LIST=()
+case "$1" in
   *w*)
-    APP_NAME="pilot-www"
+    APP_NAME_LIST+=("pilot-www")
     ;;
   *a*)
-    APP_NAME="pilot-adm"
+    APP_NAME_LIST+=("pilot-adm")
     ;;
   -h)
     echo "Usage: ${0##*/} [w|a]"
     exit 0;
     ;;
   *)
-    exit -1;
+    APP_NAME_LIST+=(
+      "pilot-www"
+      "pilot-adm"
+    )
     ;;
 esac
 
 
 printf '%s\n' $(cat << EOF
-APP_NAME=${APP_NAME}
+APP_NAME_LIST=${APP_NAME_LIST[*]}
 EOF
 )
 
