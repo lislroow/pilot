f6f19e57675955262c7fbc9c34cc30946e105b8b
김명구 <hi@mgkim.net>
2022-02-06 03:18:46 +0900

    (add) 예제코드 정리

--- framework/framework-online/src/main/java/mgkim/framework/online/v1/ExamController.java
+++ framework/framework-online/src/main/java/mgkim/framework/online/v1/ExamController.java
@@ -1,7 +1,14 @@
 package mgkim.framework.online.v1;
 
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +27,6 @@ import mgkim.framework.core.dto.KOutDTO;
 import mgkim.framework.core.env.KConstant;
 import mgkim.framework.core.logging.KLog;
 import mgkim.framework.core.logging.KLoggerFactory;
-import mgkim.framework.core.util.KStringUtil;
 
 @Api( tags = { KConstant.SWG_V1 } )
 @RestController
@@ -39,10 +45,51 @@ public class ExamController {
 			@RequestParam(required = true) String param1) throws Exception {
 		KOutDTO<Map> outDTO = new KOutDTO<Map>();
 		
-		{
-			klog.debug("my-label{10}", KStringUtil.toJsonNoPretty(outDTO));
+		Set<String> radix36Set = new HashSet<String>();
+		int a = 10;
+		while (a-- > 0) {
+			System.out.println();
+			UUID uuid1 = UUID.randomUUID();
+			ByteBuffer buffer1 = ByteBuffer.wrap(new byte[16]);
+			buffer1.putLong(uuid1.getMostSignificantBits());
+			buffer1.putLong(uuid1.getLeastSignificantBits());
+			
+			BigInteger bint = new BigInteger(1, buffer1.array());
+			UUID uuid2 = UUID.fromString(bint.toString(16).replaceFirst(
+					"([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]+)",
+					"$1-$2-$3-$4-$5"));
+			klog.debug("uuid1{15}", uuid1 + "");
+			klog.debug("uuid2{15}", uuid2 + "");
+			
+			String radix10 = bint.toString(10);
+			String radix16 = bint.toString(16);
+			String radix36 = bint.toString(36);
+			klog.debug("radix10{15}", radix10);
+			klog.debug("radix16{15}", radix16);
+			klog.debug("radix36{15}", radix36);
+			
+			radix36Set.add(radix36);
+			
+			BigInteger bint2 = new BigInteger(radix36, 36);
+			UUID uuid3 = UUID.fromString(bint2.toString(16).replaceFirst(
+					"([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]+)",
+					"$1-$2-$3-$4-$5"));
+			klog.debug("uuid3{15}", uuid3 + "");
 		}
 		
+		klog.debug("size(){15}", radix36Set.size() + "");
+		klog.debug("length()!=25{15}", radix36Set.stream().filter(s -> s.length() != 25).count() + "");
+		
+		Map<Integer, List<String>> result = radix36Set.stream().collect(HashMap<Integer, List<String>>::new,
+				(map, str) -> {
+					Integer key = str.length();
+					List<String> list = map.get(key);
+					if (list == null) {
+						list = new ArrayList<String>();
+						map.put(key, list);
+					}
+					list.add(str);
+				}, Map::putAll);
 		return outDTO;
 	}
 }
